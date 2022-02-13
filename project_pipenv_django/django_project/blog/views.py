65566e6393b1614650fe7ace49eb2e14c1c71710
Jawad Mirza <mirzamdjawad@outlook.com>
2022-02-13 01:36:39 -0800

    created home,about contactus page views & urls

--- project_pipenv_django/django_project/blog/views.py
+++ project_pipenv_django/django_project/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	return HttpResponse('<h1>Jawad Home Page Django!!!</h1>')
+
+def about(request):
+	return HttpResponse('<h1> About Page Django!!!</h1>')
+
+def contactus(request):
+	return HttpResponse('<h1> Contact Us Page Django!!!</h1>')
+
--- project_pipenv_django/django_project/django_project/urls.py
+++ project_pipenv_django/django_project/django_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', blog_views.home, name='blog-home'),
+    path('about/', blog_views.about, name='blog-about'),
+    path('contactus/', blog_views.contactus, name='blog-contactus'),
 ]
