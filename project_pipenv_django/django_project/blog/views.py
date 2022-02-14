0b811153af0baba1a71f1156648872c36baae792
Jawad Mirza <mirzamdjawad@outlook.com>
2022-02-14 01:09:40 -0800

    integrated bootstrap,static files,main.CSS...

--- project_pipenv_django/django_project/blog/views.py
+++ project_pipenv_django/django_project/blog/views.py
@@ -2,32 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-posts = [
- 	{
-		'author':'John',
-		'title':'post 1',
-		'content':'contents for blog post1',
-		'date_posted':'feb 9, 2022'	
- 	}
-
-	{
-		'author':'John1',
-		'title':'post 2',
-		'content':'contents for blog post2',
-		'date_posted':'feb 10, 2022'	
- 	}	
-
-]
 
 
 
 
 
-def home(request):
-	context = {
-		'posts': posts
-	}
-	return render(request, 'blog/home.html', context)
+
+def home(request):	
+	return render(request, 'blog/home.html', {'title' :'Home'})
 
 def about(request):
 	return render(request, 'blog/about.html', {'title' :'About'} )
