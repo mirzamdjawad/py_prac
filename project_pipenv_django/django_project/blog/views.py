8bd65833c8afb3502cf8a29a2393d435d8982f6a
Jawad Mirza <mirzamdjawad@outlook.com>
2022-02-13 23:19:47 -0800

    Getting error in view.py practice Day16 session

--- project_pipenv_django/django_project/blog/views.py
+++ project_pipenv_django/django_project/blog/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+ 	{
+		'author':'John',
+		'title':'post 1',
+		'content':'contents for blog post1',
+		'date_posted':'feb 9, 2022'	
+ 	}
+
+	{
+		'author':'John1',
+		'title':'post 2',
+		'content':'contents for blog post2',
+		'date_posted':'feb 10, 2022'	
+ 	}	
+
+]
+
+
+
+
+
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title' :'About'} )
 
 def contactus(request):
-	return render(request, 'blog/contactus.html')
+	return render(request, 'blog/contactus.html', {'title' :'Contact US'})
 
