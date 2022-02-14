2cb7f35ed7baa919a25a094db903c4660d5d750b
Jawad Mirza <mirzamdjawad@outlook.com>
2022-02-13 17:05:03 -0800

    Created templates folder & html files home,about & contactus. updated blog views.py & main settings.py file

--- project_pipenv_django/django_project/blog/views.py
+++ project_pipenv_django/django_project/blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>Jawad Home Page Django!!!</h1>')
+	return render(request, 'blog/home.html')
 
 def about(request):
-	return HttpResponse('<h1> About Page Django!!!</h1>')
+	return render(request, 'blog/about.html')
 
 def contactus(request):
-	return HttpResponse('<h1> Contact Us Page Django!!!</h1>')
+	return render(request, 'blog/contactus.html')
 
